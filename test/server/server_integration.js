1bf42ae17450a576467c4af672456264aa2016d8
alexander97olsson <alexander93olsson@hotmail.com>
2021-09-14 09:56:52 +0100

    testing

--- test/server/server_integration.js
+++ test/server/server_integration.js
@@ -13,12 +13,25 @@ chai.use(chaiHttp);
 
 describe('app', () => {
     describe('GET /data', () => {
+        it('should add an document', async function() {
+            let doc = {
+                title: "testing title",
+                maintext: "<p>Testar en string från test</p>"
+            };
+            chai.request(server)
+                .post("/data")
+                .send(doc)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                });
+        });
+
         it('Should give me some data', async function() {
             chai.request(server)
                 .get("/data")
                 .end((err, res) => {
                     res.body.should.be.an("object");
-                    res.body.data.should.be.an("array");
+                    res.body.data.msg.should.be.an("array");
                     res.body.data.msg.length.should.be.above(0);
                 });
         });
