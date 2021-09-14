7343e366570a4672b5205238d015fd0dd53a8a26
alexander97olsson <alexander93olsson@hotmail.com>
2021-09-14 08:23:43 +0100

    testing

--- test/server/server_integration.js
+++ test/server/server_integration.js
@@ -12,6 +12,17 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('app', () => {
+    describe('GET /data', () => {
+        it('Should give me some data', (done) => {
+            chai.request(server)
+                .get("/data")
+                .end((err, res) => {
+                    res.body.should.be.an("object");
+                    res.body.data.msg.should.be.an("array");
+                    res.body.data.msg.length.should.be.above(0);
+                    done();
+                });
+        });
     describe('GET /index', () => {
         it('index', (done) => {
             chai.request(server)
