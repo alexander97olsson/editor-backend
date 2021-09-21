4d57b10eb334f4a0aaeecc393afb988488551268
alexander97olsson <alexander93olsson@hotmail.com>
2021-09-21 13:24:23 +0100

    added socket io

--- app.js
+++ app.js
@@ -9,6 +9,38 @@ const app = express();
 
 const port = process.env.PORT || 1337;
 
+const httpServer = require("http").createServer(app);
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    //origin: "http://localhost:4200",
+    origin: "https://www.student.bth.se",
+    methods: ["GET", "POST"]
+  }
+});
+
+let documents = {};
+// Server
+io.sockets.on('connection', function(socket) {
+    console.log(socket.id);
+    let previousId;
+    socket.on('joinRoom', function(room) {
+        socket.leave(previousId);
+        socket.join(room);
+        console.log("saved doc: " + documents[room]);
+        if (documents[room]) {
+            io.to(room).emit("getDoc", documents[room]);
+        }
+        previousId = room;
+        
+    });
+    socket.on("doc", (data) => {
+        io.to(data["_id"]).emit("doc", data);
+        documents[data._id] = data.html;
+    });
+});
+
+
 // don't show the log when it is test
 if (process.env.NODE_ENV !== 'test') {
     // use morgan to log at command line
@@ -34,14 +66,16 @@ app.use('/data', data);
 // Put this last
 app.use((req, res, next) => {
     var err = new Error("Not Found");
-
+    
     err.status = 404;
     next(err);
 });
 
-
+//httpServer.listen(serverPort, () => {
+  //  console.log('Listening on port 1338');
+//});
 // Start up server
 //app.listen(port, () => console.log(`Example API listening on port ${port}!`));
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = httpServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = server;
